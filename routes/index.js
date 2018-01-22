6357922abb62f711970a1f7525d9eb5bccd47adc
Mattia Parisi <mattia_parisi@hotmail.it>
2018-01-22 12:36:18 +0100

    edit my file

--- routes/index.js
+++ routes/index.js
@@ -9,6 +9,14 @@ var primoMiddleware = function(req,res,next){
   }
 }
 
+/*router.use(function(req,res,next){
+  if(req.query.token == 'pippococaina'){
+    next();
+  }else {
+    res.status(401)send({message : 'Autentication failed'});
+  }
+})*/
+
 router.get ('/', primoMiddleware, function(req, res, next) {
   res.status(200).send({message : 'Autentication succeded'})
 })

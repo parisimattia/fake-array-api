6b2541b2287e5c6a3f2fc4f5360674fa6f0777fc
Mattia Parisi <mattia_parisi@hotmail.it>
2018-01-22 12:08:20 +0100

    edit my file

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,18 @@
 var express = require ('express');
 var router = express.Router ();
 
-router.get ('/', function(req,res){
-  res.status(200).json({ message: 'Ciao'});
-})
+var primoMiddleware = function(req,res,next){
+  if(req.query.token == 'pippo1'){
+    next();
+  }else {
+    res.send(401,'Autentication failed');
+  }
+}
+
+var secondoMiddleware = function(req, res, next) {
+  res.send(200, 'Autentication succeded')
+}
+
+router.get ('/', primoMiddleware, secondoMiddleware)
+
 module.exports = router;
